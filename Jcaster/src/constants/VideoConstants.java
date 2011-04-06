af0e37108c80d26a9f8b7894c2640fd291cab32b
petri.tuononen <petri.tuononen@cfb611e9-45ef-7852-107d-a28c51196201>
2011-04-06 04:44:30 +0000

    Moved Jcaster to trunk.

---
+++ Jcaster/src/constants/VideoConstants.java
@@ -0,0 +1,26 @@
+//    Copyright (C) 2011  Petri Tuononen
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package constants;
+
+//  Native Java Classes
+import java.awt.Toolkit;
+
+public class VideoConstants
+{
+    //  Constants
+    public static final int SCREEN_WIDTH = (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+    public static final int SCREEN_HEIGHT = (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+    
+}
