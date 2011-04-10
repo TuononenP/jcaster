a4b3c05a98ea0a196c0ac1a74f82078904705592
petri.tuononen <petri.tuononen@gmail.com>
2011-04-10 05:36:45 +0000

    Added Vaadin web project.

---
+++ Jcaster_vaadin/src/jcaster_vaadin/Jcaster_vaadinApplication.java
@@ -0,0 +1,17 @@
+package jcaster_vaadin;
+
+import com.vaadin.Application;
+import com.vaadin.ui.*;
+
+public class Jcaster_vaadinApplication extends Application {
+
+	private static final long serialVersionUID = 847113664237171205L;
+
+	@Override
+	public void init() {
+		Window mainWindow = new Window("Jcaster");
+		mainWindow.addComponent(new Gui());
+		setMainWindow(mainWindow);
+	}
+
+}
